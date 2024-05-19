e93ec43966b39d8ab61d8e5f39ac20dad7aa0473
Leandro Rocha <leandro.wrocha98@gmail.com>
2024-05-18 22:41:13 -0300

    aula: banco de dados

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,19 @@
  */
 
 import fastify from "fastify";
+import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
-app.get("/hello", () => {
-  return "Hello world";
+app.get("/hello", async () => {
+  const transaction = await knex("transacitons").insert({
+    id: crypto.randomUUID(),
+    title: "Transação de teste",
+    amount: "./src"
+  });
 });
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP Server Running!");
 });
